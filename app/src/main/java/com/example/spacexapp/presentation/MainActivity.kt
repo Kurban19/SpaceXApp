dce22bb9901a194ea645ea7954350b86143ea243
Kurban <imangazaliev.kb@gmail.com>
2023-04-12 17:14:47 +0300

    initial commit

---
+++ app/src/main/java/com/example/spacexapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spacexapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spacexapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
